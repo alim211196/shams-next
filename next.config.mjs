4e5b428516d1563c577e07e5af8143b2cd62eefd
Alim-Oxcytech <alim.mohd@oxcytech.com>
2025-04-11 20:10:28 +0530

    css

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,16 @@ const nextConfig = {
   images: {
     domains: ["project-images-development.s3.me-central-1.amazonaws.com"],
   },
-  webpack(config) {
+  webpack(config, { isServer }) {
+    // Fix asset path resolution
     config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
+      test: /\.(png|jpg|jpeg|gif|svg)$/i,
       type: "asset",
       generator: {
         filename: "static/media/[name].[hash][ext]",
       },
     });
+
     return config;
   },
 };
