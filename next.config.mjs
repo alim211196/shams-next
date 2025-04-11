bb59d5b154f011534d71580f9935d523f118bc80
Alim-Oxcytech <alim.mohd@oxcytech.com>
2025-04-11 21:10:17 +0530

    added config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
   images: {
     domains: ["project-images-development.s3.me-central-1.amazonaws.com"],
   },
