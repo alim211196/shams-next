a27d3704dfda3098c4d4d1568cd8ca5891058834
Alim-Oxcytech <alim.mohd@oxcytech.com>
2025-04-11 18:23:52 +0530

    changes done

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["project-images-development.s3.me-central-1.amazonaws.com"],
+    domains: [
+      "project-images-development.s3.me-central-1.amazonaws.com",
+      "lms-shams.netlify.app",
+    ],
+    unoptimized: false, // Keep image optimization unless overriding
+  },
+  webpack(config, { isServer }) {
+    // Fix asset path resolution
+    config.module.rules.push({
+      test: /\.(png|jpg|jpeg|gif|svg)$/i,
+      type: "asset",
+      generator: {
+        filename: "static/media/[name].[hash][ext]",
+      },
+    });
+
+    return config;
   },
 };
 
