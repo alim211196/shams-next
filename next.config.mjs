e3c06e2728d8c574f35ce663ce7b5c10c004914c
Alim-Oxcytech <alim.mohd@oxcytech.com>
2025-04-11 20:09:18 +0530

    update changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: ["project-images-development.s3.me-central-1.amazonaws.com"],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      type: "asset",
+      generator: {
+        filename: "static/media/[name].[hash][ext]",
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
